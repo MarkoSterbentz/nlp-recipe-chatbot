2a1511f17c0afc3d22475ac996ea0faa7ae3b0f8
Marko Sterbentz <stermark@isu.edu>
2020-03-09 02:08:18 -0500

    Stubbed out a basic, first pass for the chatbot.

--- InterfaceManager.py
+++ InterfaceManager.py
@@ -7,8 +7,11 @@ class InterfaceManager:
     Manages user interaction with the recipe transformer.
     '''
 
-    def __init__(self):
-        pass
+    def __init__(self, current_recipe=None, current_recipe_step=None, waiting_for_answer=False, terminate=False):
+        self.current_recipe = current_recipe
+        self.current_recipe_step = current_recipe_step
+        self.waiting_for_answer = waiting_for_answer
+        self.terminate = terminate
 
     def start_interaction_loop(self):
         '''
@@ -16,89 +19,97 @@ class InterfaceManager:
         :return: None
         '''
 
-        print('Welcome to the Recipe Transformer!')
+        # Print an initial welcome message
+        print('Hi! I\'m Remy! Are you ready to cook?')
+        self.waiting_for_answer = True
 
-        # Get the user input for the recipe url
+        while not self.terminate:
+            # Parse the input
+            user_input = input()
+
+            # Determine the intent of the input and the action to be taken
+            action_string, arg = self.determine_intent(user_input)
+
+            # Perform the action
+            if action_string == 'display_ingredient_list':
+                self.action_display_ingredient_list()
+            elif action_string == 'go_to_step':
+                n = 0 # TODO: Determine what step the user is asking for based on the arg and the current_step_index
+                self.action_go_to_step(n)
+            elif action_string == 'answer_how_to':
+                self.action_answer_how_to(arg)
+            elif action_string == 'answer_what_is':
+                self.action_answer_what_is(arg)
+            elif action_string == 'terminate':
+                self.terminate = True
+            else:
+                # Don't know what they're saying.
+                print('Sorry, I didn\'t catch that. Could you try rephrasing that?')
+
+        print('Thanks for talking with me today!')
+
+        return
+
+    def determine_intent(self, s):
+        '''
+        Determines the user's intent in a given string and returns the proper action to be taken.
+        :param s: The string of user input.
+        :return: The action to be taken based on the intent detected in the string, as well as the argument required for
+        that action to be done properly.
+        TODO: This is (I think) where RASA will be deployed.
+        '''
+        if s == 'quit':
+            return 'terminate', None
+        return None, None
+
+    def action_get_url(self):
+        '''
+        Asks the user for a recipe URL from AllRecipes.com.
+        :return: Returns the recipe object, if it was a valid URL
+        '''
         original_recipe = None
         while original_recipe is None:
-            url = input('Please enter a URL from AllRecipes.com: ')
+            url = input('Alrighty, give me a recipe from AllRecipes.com: ')
 
             # Initialize a Recipe object based on this recipe
             original_recipe = rp.get_recipe(url)
 
             if original_recipe is None:
-                print('Unable to find a recipe at the provided URL.\n')
+                print('Sorry, I wasn\'t able to find a recipe there.\n')
 
-        # Store the current recipe to be transformed by the next user input
-        current_recipe = original_recipe
-        substitutions_performed = {}
-
-        # Print out the recipe that was found
-        print('\nFound the following recipe:\n')
-        print(current_recipe)
-
-        # Start the main user input loop
-        terminate = False
-        while not terminate:
-            # Provide options to the user
-            self.__print_transformation_options()
-
-            # Parse in the option selected by the user
-            selected_option = input('To perform an action, please input the number associated with the appropriate option: ')
-
-            # Perform the action
-            if selected_option == '0':
-                current_recipe, substitutions_performed = current_recipe.transform_healthy()
-                print("\nThe new, more healthy recipe is below: \n".upper())
-                print(current_recipe.__str__())
-                self.__print_substitutions_performed(substitutions_performed)
-            elif selected_option == '1':
-                current_recipe, substitutions_performed = current_recipe.transform_unhealthy()
-                print("\nThe new, less healthy recipe is below: \n".upper())
-                print(current_recipe.__str__())
-                self.__print_substitutions_performed(substitutions_performed)
-            elif selected_option == '2':
-                current_recipe, substitutions_performed = current_recipe.transform_vegetarian()
-                print("\nThe new, vegetarian version of the recipe is below: \n".upper())
-                print(current_recipe.__str__())
-                self.__print_substitutions_performed(substitutions_performed)
-            elif selected_option == '3':
-                current_recipe, substitutions_performed = current_recipe.transform_non_vegetarian()
-                print("\nThe new, non-vegetarian version of the recipe is below: \n".upper())
-                print(current_recipe.__str__())
-                self.__print_substitutions_performed(substitutions_performed)
-            elif selected_option == '4':
-                current_recipe, substitutions_performed = current_recipe.transform_cuisine('japan')
-                print("\nThe new, more Japanese version of the recipe is below: \n".upper())
-                print(current_recipe.__str__())
-                self.__print_substitutions_performed(substitutions_performed)
-            elif selected_option == '5':
-                current_recipe, substitutions_performed = current_recipe.transform_cuisine('mexico')
-                print("\nThe new, more Mexican version of the recipe is below: \n".upper())
-                print(current_recipe.__str__())
-                self.__print_substitutions_performed(substitutions_performed)
-            elif selected_option == '6':
-                current_recipe, substitutions_performed = current_recipe.transform_cuisine('italy')
-                print("\nThe new, more Italian version of the recipe is below: \n".upper())
-                print(current_recipe.__str__())
-                self.__print_substitutions_performed(substitutions_performed)
-            elif selected_option == '7':
-                current_recipe = current_recipe.transform_size(2.0)
-                print("\nThe new, double portion version of the recipe is below: \n".upper())
-                print(current_recipe.__str__())
-            elif selected_option == '8':
-                current_recipe = current_recipe.transform_size(0.5)
-                print("\nThe new, half portion version of the recipe is below: \n".upper())
-                print(current_recipe.__str__())
-            elif selected_option == '9':
-                terminate = True
-            else:
-                print('Invalid input. Please input a number between 0 and 6.')
-
-        print('Thank you for using our recipe transformer!')
-
+    def action_display_ingredient_list(self):
+        '''
+        Displays the ingredient list of the current recipe.
+        :return: None
+        '''
         return
 
+    def action_go_to_step(self, n):
+        '''
+        Displays the nth step of the current recipe, if it exists.
+        :param n: The index of the step to display.
+        :return: None
+        '''
+        self.current_recipe_step = n
+        return
+
+    def action_answer_how_to(self, action):
+        '''
+        Displays the search results from YouTube for 'how to <action>?'
+        :param action: The action to search Youtube how to do.
+        Note: Queries on YouTube have the form: https://www.youtube.com/results?search_query=how+to+<action>
+        :return: None
+        '''
+        return
+
+    def action_answer_what_is(self, thing):
+        '''
+        Displays the search results from Google for 'what is <thing>?'
+        :param thing: The thing to search Google for what it is.
+        Note: Queries on Google have the form: https://www.google.com/search?q=what+is+<thing>
+        :return: None
+        '''
+        return
 
     def __print_transformation_options(self):
         '''
